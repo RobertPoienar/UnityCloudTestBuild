80b206a4ac13643d63aaf6616d2e41e58ac81350
Robert Poienar <robert.poienar@altom.com>
2025-09-25 18:05:50 +0300

    Added build script

---
+++ Assets/Editor/UnityCloudBuildScript.cs
@@ -0,0 +1,30 @@
+using AltTester.AltTesterSDK.Driver;
+using AltTester.AltTesterUnitySDK.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AltTester.BuildScripts
+{
+    public class UnityCloudBuildScript
+    {
+        public static void OnPreExportWindows()
+        {
+            Debug.Log("Unity Cloud Build - OnPreExportWindows called");
+
+            var buildTargetGroup = BuildTargetGroup.Standalone;
+
+            if (buildTargetGroup == UnityEditor.BuildTargetGroup.Standalone)
+            {
+                AltBuilder.CreateJsonFileForInputMappingOfAxis();
+            }
+            var instrumentationSettings = new AltInstrumentationSettings();
+            instrumentationSettings.AltServerHost = "127.0.0.1";
+            instrumentationSettings.AltServerPort = 13000;
+            instrumentationSettings.AppName = "__default__";
+            instrumentationSettings.ResetConnectionData = true;
+            AltBuilder.InsertAltInScene(SceneManager.GetSceneByBuildIndex(0).path, instrumentationSettings);
+        }
+    }
+}
+
